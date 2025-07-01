be125c335739d17f05321b3657ab080c3d9678c3
changs97 <kin2027@gmail.com>
2025-07-01 23:08:44 +0900

    feat: 로그인 화면 UI 구현

--- feature/login/src/main/java/com/combo/runcombi/feature/login/LoginScreen.kt
+++ feature/login/src/main/java/com/combo/runcombi/feature/login/LoginScreen.kt
@@ -1,18 +1,34 @@
 package com.combo.runcombi.feature.login
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.combo.runcombi.core.designsystem.component.StableImage
+import com.combo.runcombi.core.designsystem.theme.Grey01
+import com.combo.runcombi.core.designsystem.theme.RunCombiTypography
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -59,14 +75,47 @@ fun LoginScreen(
 ) {
     Box(
         modifier = modifier
+            .background(Grey01)
+            .fillMaxSize()
     ) {
-        Column(
-            modifier = modifier
-                .fillMaxSize()
-                .navigationBarsPadding(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+        Text(
+            text = "RUNCOMBI",
+            color = Color(0xFFD7FE63),
+            fontSize = 40.sp,
+            fontWeight = FontWeight.ExtraBold,
+            modifier = Modifier
+                .align(Alignment.Center)
+        )
 
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 39.dp, start = 20.dp, end = 20.dp)
+                .align(Alignment.BottomCenter)
+        ) {
+            Button(
+                onClick = onKakaoLoginClick,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFFEE102),
+                    contentColor = Color.Black
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp),
+                shape = RoundedCornerShape(6.dp)
+            ) {
+                StableImage(
+                    drawableResId = R.drawable.ic_kakao,
+                    description = "카카오톡",
+                    modifier = Modifier.size(22.dp)
+                )
+                Text(
+                    text = "카카오로 시작하기",
+                    style = RunCombiTypography.body1,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.weight(1f)
+                )
+            }
         }
     }
 }
