80753c534e65d311fc0c3c04671861d921624c6f
changs97 <kin2027@gmail.com>
2025-07-01 23:08:05 +0900

    feat: 디자인 시스템 Type.kt 폰트 스타일 수정 및 Image 컴포넌트 추가

--- core/designsystem/src/main/java/com/combo/runcombi/core/designsystem/theme/Type.kt
+++ core/designsystem/src/main/java/com/combo/runcombi/core/designsystem/theme/Type.kt
@@ -155,4 +155,53 @@ object RunCombiTypography {
     val tab_regular = tab.copy(
         fontWeight = FontWeight.Normal
     )
+
+    // Heading 1
+    val heading1 = DefaultTextStyle.copy(
+        fontSize = 28.sp,
+        lineHeight = 40.sp,
+        fontWeight = FontWeight.SemiBold
+    )
+
+    // Title 2
+    val title2 = DefaultTextStyle.copy(
+        fontSize = 22.sp,
+        lineHeight = 34.sp,
+        fontWeight = FontWeight.SemiBold
+    )
+
+    // Title 3
+    val title3 = DefaultTextStyle.copy(
+        fontSize = 20.sp,
+        lineHeight = 32.sp,
+        fontWeight = FontWeight.SemiBold
+    )
+
+    // Title 4
+    val title4 = DefaultTextStyle.copy(
+        fontSize = 18.sp,
+        lineHeight = 30.sp,
+        fontWeight = FontWeight.SemiBold
+    )
+
+    // Body 1
+    val body1 = DefaultTextStyle.copy(
+        fontSize = 16.sp,
+        lineHeight = 26.sp,
+        fontWeight = FontWeight.Medium
+    )
+
+    // Body 2
+    val body2 = DefaultTextStyle.copy(
+        fontSize = 14.sp,
+        lineHeight = 24.sp,
+        fontWeight = FontWeight.Medium
+    )
+
+    // Body 3
+    val body3 = DefaultTextStyle.copy(
+        fontSize = 12.sp,
+        lineHeight = 22.sp,
+        fontWeight = FontWeight.SemiBold
+    )
 }
