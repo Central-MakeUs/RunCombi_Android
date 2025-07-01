e5772573f55bc0090885f6fa65f26c2582683ca7
changs97 <kin2027@gmail.com>
2025-07-01 22:15:16 +0900

    feat: 디자인 시스템(테마/컬러/폰트) 세팅

--- core/designsystem/src/main/java/com/combo/runcombi/core/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/combo/runcombi/core/designsystem/theme/Color.kt
@@ -2,10 +2,28 @@ package com.combo.runcombi.core.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// 기본 색상
+val Black00 = Color(0xFF0D0D0D)
+val WhiteFF = Color(0xFFF4F4F4)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Primary
+val Primary01 = Color(0xFFD7FFE6)
+val Primary02 = Color(0xFFE8FFA3)
+val Primary03 = Color(0xFF6C774C)
+val Primary04 = Color(0xFF515E26)
+
+// Sub
+val Sub01 = Color(0xFFE994F3)
+val Sub02 = Color(0xFFBCB3F6)
+val Sub03 = Color(0xFFC352D5)
+val Sub04 = Color(0xFF8121CD)
+
+// Greyscale
+val Grey01 = Color(0xFF171717)
+val Grey02 = Color(0xFF252525)
+val Grey03 = Color(0xFF3B3B3B)
+val Grey04 = Color(0xFF525252)
+val Grey05 = Color(0xFF757575)
+val Grey06 = Color(0xFF999999)
+val Grey07 = Color(0xFFB3B3B3)
+val Grey08 = Color(0xFFEDEDED)
--- core/designsystem/src/main/java/com/combo/runcombi/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/combo/runcombi/core/designsystem/theme/Theme.kt
@@ -11,15 +11,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Primary01,
+    secondary = Sub01,
+    tertiary = Grey01
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Primary01,
+    secondary = Sub01,
+    tertiary = Grey01
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -33,11 +33,11 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun RunCombi_AndroidTheme(
+fun RunCombiTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
@@ -51,7 +51,6 @@ fun RunCombi_AndroidTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
         content = content
     )
 }
