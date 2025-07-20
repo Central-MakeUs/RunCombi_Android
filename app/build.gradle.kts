fce1b496cfa5d4368f33d43a5985393b7009806d
changs97 <kin2027@gmail.com>
2025-07-20 20:16:02 +0900

    release: versionName 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.combo.runcombi"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
+        versionCode = 100
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
